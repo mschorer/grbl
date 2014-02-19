3565993f2641cdcfce9df1c24c11b04e1b57d382
mschorer <ms@ms-ite.de>
2014-02-19 18:32:26 +0100

    - i2c latest

--- defaults.h
+++ defaults.h
@@ -54,10 +54,10 @@
   #define DEFAULT_INVERT_ST_ENABLE 0 // false
   #define DEFAULT_INVERT_LIMIT_PINS 0 // false
   #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
-  #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
-  #define DEFAULT_HOMING_ENABLE 0  // false
-  #define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
-  #define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
+  #define DEFAULT_HARD_LIMIT_ENABLE 1  // false
+  #define DEFAULT_HOMING_ENABLE 1  // false
+  #define DEFAULT_HOMING_DIR_MASK ((1<<Z_DIRECTION_BIT)) // move positive dir
+  #define DEFAULT_HOMING_FEED_RATE 50.0 // mm/min
   #define DEFAULT_HOMING_SEEK_RATE 250.0 // mm/min
   #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
   #define DEFAULT_HOMING_PULLOFF 1.0 // mm
